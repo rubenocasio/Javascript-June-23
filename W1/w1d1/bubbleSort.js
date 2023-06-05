565d9d52c5c7c8a2dc083aed5dba4e457fe5ec1c
Ruben Ocasio <rocasio@codingdojo.com>
2023-06-05 14:32:59 -0400

    Update bubbleSort.js

--- W1/w1d1/bubbleSort.js
+++ W1/w1d1/bubbleSort.js
@@ -24,11 +24,61 @@
  * @param {Array<number>} nums
  * @returns {Array<number>} The given nums after being sorted.
  */
+/**
+ * Sorts the given nums in-place by mutating the array.
+ * Best: O(n) linear when array is already sorted.
+ * Average: O(n^2) quadratic.
+ * Worst: O(n^2) quadratic when the array is reverse sorted.
+ * @param {Array<number>} nums
+ * @returns {Array<number>} The given nums after being sorted.
+ */
 const bubbleSort = (nums) => {
-    //Code goes here
+    // Initialize a flag to track if the array is sorted
+    let isSorted = false;
+
+    // Keep looping until the array is sorted
+    while (!isSorted) {
+        // Assume the array is sorted at the beginning of each iteration
+        isSorted = true;
+        // Iterate through the array, comparing adjacent elements
+        for (let i = 0; i < nums.length - 1; i++) {
+            // If the current element is greater than the next one, swap them
+            if (nums[i] > nums[i + 1]) {
+                // Use array destructuring to swap the elements in a concise and readable way
+                [nums[i], nums[i + 1]] = [nums[i + 1], nums[i]];
+                // Since we had to swap elements, set isSorted to false to indicate the array may not be fully sorted yet
+                isSorted = false;
+            }
+        }
+    }
+    // After the while loop has completed, the array is sorted; return the sorted array
+    return nums;
 }
+
 const numsRandomOrder = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
 console.log(bubbleSort(numsRandomOrder));
 
 
-/*****************************************************************************/
+const bubbleSort2 = (nums) => {
+    const len = nums.length;
+  
+    for (let i = 0; i < len - 1; i++) {
+
+      // Last i elements are already in place
+      for (let j = 0; j < len - i - 1; j++) {
+
+        // Compare adjacent elements
+        if (nums[j] > nums[j + 1]) {
+
+          // Swap if the left element is larger than the right element
+          [nums[j], nums[j + 1]] = [nums[j + 1], nums[j]];
+        }
+      }
+    }
+    return nums;
+  };
+  
+  const numsRandomOrder2 = [9, 2, 5, 6, 4, 3, 7, 10, 1, 8];
+  console.log(bubbleSort2(numsRandomOrder));
+
+  module.exports = { bubbleSort, bubbleSort2 };
