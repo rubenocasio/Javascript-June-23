70756a306a5b5bf58aa13969025d251ed485f2e1
Ruben Ocasio <rocasio@codingdojo.com>
2023-06-05 13:04:57 -0400

    Update jsFundamentals.js

--- W1/w1d1/jsFundamentals.js
+++ W1/w1d1/jsFundamentals.js
@@ -3,10 +3,25 @@
 // ------------- Javascript history -------------
 
 // ------------- Scope ------------
+var global = "Hi, my name is Ruben"
+var r = "Ronnie Rios"
+function sayName(){
+    console.log(global)
+}
+// console.log(r) // undefined
+// sayName() // Hi my name
 
-// -------------- Hoisting ------------
+
+function stack(){
+    var local = "MERN STACK RULES"
+    console.log(local)
+}
+console.log(local)//undefined
+stack()// "MERN STACK RULES"
 
 // -------------- Const vs Let vs Var --------------
+
+// -------------- Hoisting ------------
 
 // -------------- Destructuring --------------
 
