d0d791f5d04219ee5ee69d9946cd5bed3dc063d5
Ruben Ocasio <rocasio@codingdojo.com>
2023-06-06 12:59:10 -0400

    Create jsFunctionalProgram.js

---
+++ W1/w1d2/jsFunctionalProgram.js
@@ -0,0 +1,13 @@
+//------ callback functions -------
+
+// ------ Object.freeze() -------
+const shoppingList = Object.freeze([
+    { id: 3, itemName: "Dog Food", price: 1.29, shops: ["Petco", "Trader Joes"] },
+    { id: 5, itemName: "Logitech Gameing Chair", price: 1500, shops: ["Amazon", "Best Buy"]},
+    { id: 9, itemName: "Soup Dumplings", price: 3.29, shops: ["Trader Joes"] },
+    { id: 2, itemName: "Air ticket to Japan", price: 1400, shops: ["Expedia"] },
+    { id: 10, itemName: "Custard Lemon Tart", price: 3.99, shops: ["Trader Joes"] },
+]);
+
+// ------ .map & .filter -------
+
