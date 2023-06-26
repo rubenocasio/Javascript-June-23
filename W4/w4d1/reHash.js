d2d20e5c4abf0695d487e28552af7bef198490b3
Ruben Ocasio <rocasio@codingdojo.com>
2023-06-26 09:15:31 -0400

    Create reHash.js

---
+++ W4/w4d1/reHash.js
@@ -0,0 +1,21 @@
+/* 
+    Given by Riot games. Rehash an incorrectly hashed string 
+    by combining letter count occurrences and alphabetizing them.
+*/
+
+const str1 = "b70a164c32a20c10j3a15";
+const expected1 = "a199b70c42j3";
+
+/**
+ * Time: O(?).
+ * Space: O(?).
+ * @param {string} s An incorrectly hashed string.
+ * @returns {string} The correctly rehashed string alphabetized.
+ */
+function rehash(s) {
+    //Code block
+}
+
+/*****************************************************************************/
+
+module.exports = { rehash };
