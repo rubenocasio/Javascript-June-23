df7dab32dd38bea59247dcf0fbee8a870c9979c4
Ruben Ocasio <rocasio@codingdojo.com>
2023-06-28 11:29:34 -0400

    Update canBuildS1FromS2.jsx

--- W4/w4d3/canBuildS1FromS2.jsx
+++ W4/w4d3/canBuildS1FromS2.jsx
@@ -2,7 +2,6 @@
 Given two strings, return true if the first string can be built from the letters in the 2nd string
 Ignore case .indexOf will only tell you if the letter is found one time
 */
-
 const strA1 = "Hello World";
 const strB1 = "lloeh wordl";
 const expected1 = true;
@@ -27,10 +26,26 @@ const expected5 = false;
 // Explanation: strB5 does not have enough "l" chars.
 
 /*****************************************************************************/
+//Loop, comparison, lowercase all
+
+
 function canBuildS1FromS2(s1, s2) {
-  //Code block
+  const charFreq = []
+
+  for(let char of s2.toLowerCase()){
+    // console.log("char: " + char)
+    charFreq[char] = charFreq[char] + 1 || 1
+    // console.log("charFreq[char]: " + charFreq[char])
+  }
+  for(let char of s1.toLowerCase()){
+    if(!charFreq[char]){
+      return false
+    }
+    charFreq[char]--
+  }
+  return true
 }
-console.log(canBuildS1FromS2(strA1, strB1));
+console.log(canBuildS1FromS2(strA1, strB1)); //true
 console.log(canBuildS1FromS2(strA2, strB2));
 console.log(canBuildS1FromS2(strA3, strB3));
 console.log(canBuildS1FromS2(strA4, strB4));
